4df813cfdf510eb1c2887c0dcdaf9d3e378d4073
Olu D Gbadebo <odgbadebo@gmail.com>
2019-08-09 15:32:53 -0700

    api supports querying for session by id

--- api.js
+++ api.js
@@ -2,9 +2,32 @@
 
 const express = require('express');
 const router = express.Router();
+const jsonfile = require('jsonfile');
+const path = require('path');
 
 router.get('/', (request, response) => {
-    
+    response.send('Connected to the API')
+});
+
+router.post('/session', (request, response) => {
+    if (!request.body.sessionID) {
+        response.status(400);
+        response.send();
+    } else {
+        let databasePath = path.join(__dirname, '/database/sessions.json');
+        let sessionID = request.body.sessionID;
+        jsonfile.readFile(databasePath)
+            .then(sessions =>{
+                let session = sessions.find(sesh => sesh.id === parseInt(sessionID));
+                session ? response.status(200) : response.status(400);
+                response.send(session);
+            })
+            .catch(error => {
+                console.error(error);
+                response.status(400);
+                response.send()
+            })
+    }
 });
 
 
