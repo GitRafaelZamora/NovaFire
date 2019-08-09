4f735cb6aa3269879dfb540151fdcb64f58d07bf
Olu D Gbadebo <odgbadebo@gmail.com>
2019-08-09 15:34:07 -0700

    added redirection to 404 page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 
 // Components
 import Editor from './components/templates/Editor'
 import Login from './components/pages/Login'
 import Dashboard from './components/pages/Dashboard'
+import Error from './components/pages/Error'
 
 // Material UI
 import { StylesProvider, createGenerateClassName } from '@material-ui/styles'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'c',
-})
+});
 
 function App() {
   return (
@@ -22,7 +22,8 @@ function App() {
         <div className="App">
           <Route exact path="/" component={Login} />
           <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/editor" component={Editor} />
+          <Route exact path="/editor/:sessionid" component={Editor} />
+          <Route path={["", "/error"]} component={Error} />
         </div>
       </Router>
     </StylesProvider>
