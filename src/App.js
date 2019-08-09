958c32aeecd4fe11daf12b79c3de935d4fd46c24
GitRafaelZamora <RafaelZamora.Me@gmail.com>
2019-08-09 09:44:47 -0700

    adds BrowserRouter to App.js and adds skeleton pages for Login, Editor, and Dashboard.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
+// Components
+import Editor from './components/Editor'
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/editor" component={Editor} />
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/Editor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Editor extends Component {
+    render() {
+        return (
+            <div>
+                Hello from Editor
+            </div>
+        )
+    }
+}
+
+export default Editor
