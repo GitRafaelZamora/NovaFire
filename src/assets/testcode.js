aaf51d11ab589b8a7921e9e68f8bb69511152612
GitRafaelZamora <RafaelZamora.Me@gmail.com>
2019-08-10 07:52:41 -0700

    adds syntax highlighting to the texteditor

---
+++ src/assets/testcode.js
@@ -0,0 +1,8 @@
+export default "" +
+"function reverseString(string) { \n" +
+  "var reversed = '';\n" +
+  "for (var i = string.length-1; i >= 0; i--) {\n" +
+    "reversed += string[i];\n" +
+  "}\n" +
+  "console.log('Reversed: ' + reversed);\n" +
+"}\n";
--- src/components/atoms/TextEditor.js
+++ src/components/atoms/TextEditor.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react'
+
+// Material UI
 import { withStyles } from '@material-ui/core';
 
-const styles = {
-  texteditor: {
-    color: 'green'
+import testcode from '../../assets/testcode'
+
+// 
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview
+} from 'react-live';
+
+const styles = theme => ({
+  document: {
+    color: 'yellow',
+    fontFamily: 'Inconsolata, monospace',
+    fontSize: '14px',
+    minHeight: '100%',
+    textAlign: 'left',
+    padding: '20px',
   }
-}
+})
 
 export class TextEditor extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '',
+    }
+  }
+
+  updateContent = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
-    const classes = this.props
+    const { classes } = this.props
+
+    var syntax = testcode;
+
     return (
-      <div className={classes.texteditor} contentEditable={true}>
-        This is the TextEditor
-      </div>
+      <LiveProvider name="code" code={this.state.code} onChange={this.updateContent} contentEditable={true}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
     )
   }
 }
