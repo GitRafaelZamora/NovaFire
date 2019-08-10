2495de577e660eb15848e7a351e6396db4042f2e
Olu D Gbadebo <odgbadebo@gmail.com>
2019-08-09 17:03:05 -0700

    setup firebase admin

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const jsonfile = require('jsonfile');
 const path = require('path');
+const firebase = require('./firebase');
 
 router.get('/', (request, response) => {
     response.send('Connected to the API')
@@ -30,5 +31,36 @@ router.post('/session', (request, response) => {
     }
 });
 
+router.post('/createUser', (request, response) => {
+    function validateNewUser(user) {
+        if (!user) return user;
+    /*
+        {
+            email: 'user@example.com',
+            emailVerified: false,
+            phoneNumber: '+11234567890',
+            password: 'secretPassword',
+            displayName: 'John Doe',
+            photoURL: 'http://www.example.com/12345678/photo.png',
+            disabled: false
+        }
+     */
+    return user.hasOwnProperty('email') &&
+        user.hasOwnProperty('password') &&
+        user.hasOwnProperty('emailVerified') &&
+        user.hasOwnProperty('displayName')
+    }
+
+    if (validateNewUser(request.body.user)) {
+        firebase.createUser(request.body.user)
+            .then(() => response.send)
+            .catch(() => badRequest(response));
+    } else badRequest(response)
+});
+
+function badRequest(response) {
+    response.status(400);
+    response.send();
+}
 
 module.exports = router;
---
+++ firebase.js
@@ -0,0 +1,31 @@
+
+let admin = require("firebase-admin");
+let path = require('path');
+
+let serviceAccount = require(path.join(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS));
+
+class Firebase {
+    constructor() {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://binate-363ac.firebaseio.com"
+        });
+    }
+
+    createUser(user) {
+        return new Promise((resolve, reject) => {
+            admin.auth().createUser(user)
+                .then(function(userRecord) {
+                    // See the UserRecord reference doc for the contents of userRecord.
+                    console.log('Successfully created new user:', userRecord.uid);
+                    resolve();
+                })
+                .catch(function(error) {
+                    console.log('Error creating new user:', error);
+                    reject();
+                });
+        })
+    }
+}
+
+module.exports = new Firebase();
